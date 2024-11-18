dcbb948a63be526713d41033d4c22cb47fa59d38
lltosso <luis.tosso@gmail.com>
2024-11-18 14:26:44 +0100

    java 1

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -3,6 +3,14 @@ public class AboutMe {
         //inicializar una variable (del tipo que veas conveniente) para cada uno de los siguentes datos: nombre, edad, país, lenguaje de programación, horas, pasatiempo, lugar y reemplaza en el texto de impresión, la variable que corresponda.
 
         //Escribe tu código aquí
+        String variable1 = "Lluis";
+
+
+        String variable2 = "99";
+
+
+        String variable3 = "España";
+      System.out.println("Soy la variable 1:" + variable1 + "Soy la variable 2" + variable2 + "Soy la variable 3" + variable3);
 
         //Reemplaza <name> y <age> concatenando el String con las variables creadas. 
         System.out.println("Mi nombre es <name>, tengo <age> años y soy de <country>.");
